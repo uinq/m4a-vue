59d7897d8472bd726d40d9dfa1edc95517cac14a
ing999 <lovebites444@gmail.com>
2021-08-18 17:09:42 +0900

    회사에서2

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,29 @@
 <template>
-  <header id="nav" class="header">
-    <div class="logo"><router-link to="/">미디어포스 얼라이언스</router-link></div>
-    <button class="btn-nav">링크열기</button>
-    <nav class="nav">
-      <router-link class="nav-link1" to="/works">WORKS</router-link>
-      <router-link class="nav-link2" to="/about">ABOUT M4A</router-link>
-      <router-link class="nav-link3" to="/contact">CONTACT</router-link>
+  <header id="header" class="header">
+    <div id="logo" class="logo"><router-link to="/">미디어포스 얼라이언스</router-link></div>
+    <a href="#gnb" class="btn-nav">메뉴열기</a>
+    <nav id="nav" class="nav">
+      <ul>
+        <li class="nav-item"><router-link class="nav-link" to="/works">WORKS</router-link></li>
+        <li class="nav-item"><router-link class="nav-link" to="/about">ABOUT M4A</router-link></li>
+        <li class="nav-item"><router-link class="nav-link" to="/contact">CONTACT</router-link></li>
+      </ul>
     </nav>
-    <div class="util-ask">
-      <dl class="detail">
-        <dt>영업문의</dt>
-        <dd>
-          <div class="email">jinny@m4a.co.kr <button class="btn-copy">COPY</button></div>
-          <div class="tel">02.536.0518(161)</div>
-        </dd>
-      </dl>
-    </div>
+
   </header>
   <main id="container" class="container">
     <router-view />
   </main>
 
-  <footer><address>ⓒ MEDIA4TH ALLIANCE CO., LTD.</address></footer>
+  <footer id="footer" class="footer">
+    <div class="sales-inquiry">
+      <h2 class="tit">영업문의</h2>
+      <div id="salesInquiry" class="detail">
+        <p class="email"><a href="mailto:jinny@m4a.co.kr">jinny@m4a.co.kr</a></p>
+        <p class="tel">02.536.0518(161)</p>
+        <button class="btn-copy">COPY</button>
+      </div>
+    </div>
+    <address>ⓒ MEDIA4TH ALLIANCE CO., LTD.</address>
+  </footer>
 </template>
