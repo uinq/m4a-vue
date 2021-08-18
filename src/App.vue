c3a626ca083a1340cdb6133b877f5354a61f3c5a
ing999 <lovebites444@gmail.com>
2021-08-18 16:09:56 +0900

    회사에서

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,25 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  <header id="nav" class="header">
+    <div class="logo"><router-link to="/">미디어포스 얼라이언스</router-link></div>
+    <button class="btn-nav">링크열기</button>
+    <nav class="nav">
+      <router-link class="nav-link1" to="/works">WORKS</router-link>
+      <router-link class="nav-link2" to="/about">ABOUT M4A</router-link>
+      <router-link class="nav-link3" to="/contact">CONTACT</router-link>
+    </nav>
+    <div class="util-ask">
+      <dl class="detail">
+        <dt>영업문의</dt>
+        <dd>
+          <div class="email">jinny@m4a.co.kr <button class="btn-copy">COPY</button></div>
+          <div class="tel">02.536.0518(161)</div>
+        </dd>
+      </dl>
+    </div>
+  </header>
+  <main id="container" class="container">
+    <router-view />
+  </main>
+
+  <footer><address>ⓒ MEDIA4TH ALLIANCE CO., LTD.</address></footer>
 </template>
